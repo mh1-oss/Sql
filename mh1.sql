3118b08b0074ca1747ae803e1b260ff9a248fd41
mh1-oss <mmhhoo967.mm@gmail.com>
2026-02-15 22:50:46 +0300

    Add files via upload

---
+++ mh1.sql
@@ -0,0 +1,24 @@
+--CREATE TABLE books (
+   -- book_id SERIAL PRIMARY KEY,        -- رقم تسلسلي تلقائي ومفتاح أساسي
+    --title VARCHAR(255) NOT NULL,       -- نص لا يمكن أن يكون فارغاً
+    --author VARCHAR(255),               -- اسم المؤلف
+    --published_year INT,                -- سنة النشر (رقم صحيح)
+    --price NUMERIC(10, 2)               -- السعر (رقم عشري، خانتين بعد الفاصلة)
+--);
+--INSERT INTO books (title, author, published_year, price)
+--VALUES 
+  --  ('The Great Gatsby', 'F. Scott Fitzgerald', 1925, 10.99),
+    --('Atomic Habits', 'James Clear', 2018, 20.00),
+    --('Clean Code', 'Robert C. Martin', 2008, 45.50);
+	
+	--SELECT * FROM books 
+--WHERE published_year > 2010;
+
+--UPDATE books
+--SET price = 25.50
+--WHERE book_id = 1;
+
+--SELECT * FROM books 
+--ORDER BY price ASC;
+
+
